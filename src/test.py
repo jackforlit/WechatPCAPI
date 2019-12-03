0cbbd1818acd2d56b918e47e888065156d2c8e5d
Leon <mocha_lee@qq.com>
2019-12-03 23:19:23 +0800

    更新 新增群成员查询接口 更新test示例

--- src/test.py
+++ src/test.py
@@ -9,12 +9,30 @@
 from WechatPCAPI import WechatPCAPI
 import time
 import logging
+from queue import Queue
+import threading
+
 
 logging.basicConfig(level=logging.INFO)
+queue_recved_message = Queue()
 
 
 def on_message(message):
-    print(message)
+    queue_recved_message.put(message)
+
+
+# 消息处理示例 仅供参考
+def thread_handle_message(wx_inst):
+    while True:
+        message = queue_recved_message.get()
+        print(message)
+        if 'msg' in message.get('type'):
+            # 这里是判断收到的是消息 不是别的响应
+            msg_content = message.get('data', {}).get('msg', '')
+            send_or_recv = message.get('data', {}).get('send_or_recv', '')
+            if send_or_recv[0] == '0':
+                # 0是收到的消息 1是发出的 对于1不要再回复了 不然会无限循环回复
+                wx_inst.send_text('filehelper', '收到消息:{}'.format(msg_content))
 
 
 def main():
@@ -24,10 +42,14 @@ def main():
     while not wx_inst.get_myself():
         time.sleep(5)
 
+    print('登陆成功')
     print(wx_inst.get_myself())
+
+    threading.Thread(target=thread_handle_message, args=(wx_inst,)).start()
 
     time.sleep(10)
     wx_inst.send_text(to_user='filehelper', msg='777888999')
+    time.sleep(1)
     wx_inst.send_link_card(
         to_user='filehelper',
         title='博客',
@@ -35,11 +57,23 @@ def main():
         target_url='http://www.honglingjin.online/',
         img_url='http://honglingjin.online/wp-content/uploads/2019/07/0-1562117907.jpeg'
     )
+    time.sleep(1)
+
     wx_inst.send_img(to_user='filehelper', img_abspath=r'C:\Users\Leon\Pictures\1.jpg')
+    time.sleep(1)
+
     wx_inst.send_file(to_user='filehelper', file_abspath=r'C:\Users\Leon\Desktop\1.txt')
+    time.sleep(1)
+
     wx_inst.send_gif(to_user='filehelper', gif_abspath=r'C:\Users\Leon\Desktop\08.gif')
+    time.sleep(1)
+
     wx_inst.send_card(to_user='filehelper', wx_id='gh_6ced1cafca19')
 
+    # 这个是获取群具体成员信息的，成员信息也从上面的回调返回
+    wx_inst.get_member_of_chatroom('18880790370@chatroom')
+
+    # 这个是更新所有好友、群、公众号信息的
     # wx_inst.update_frinds()
 
 
